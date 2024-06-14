a94d7d71c4e9ee23998f57288ba1013cca9f5803
Francis <vlfmap@gmail.com>
2024-06-14 14:41:49 +0200

    text editor stepper logic

--- src/component/text-editor/text-editor.component.tsx
+++ src/component/text-editor/text-editor.component.tsx
@@ -1,8 +1,9 @@
 import { Box } from "@mui/system";
 import { Article, ArticleLabels } from "../../type/article.type";
 import { Tabs } from "@mui/base";
-import { Tab } from "@mui/material";
+import { Button, Card, CardActions, CardContent, Step, StepButton, Stepper, Tab, Typography } from "@mui/material";
 import React from "react";
+import './text-editor.component.css';
 
 export type TextEditorProps = {
     edit?: Article,
@@ -19,9 +20,134 @@ type TextEditorPrefill = {
     labels?: Array<ArticleLabels>
 }
 
+const steps = ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+
 const TextEditor = (props: TextEditorProps) => {
+
+    const [activeStep, setActiveStep] = React.useState(0);
+    const [completed, setCompleted] = React.useState<{ [k: number]: boolean; }>({});
+
+    const totalSteps = () => {
+        return steps.length;
+    };
+
+    const completedSteps = () => {
+        return Object.keys(completed).length;
+    };
+
+    const isLastStep = () => {
+        return activeStep === totalSteps() - 1;
+    };
+
+    const allStepsCompleted = () => {
+        return completedSteps() === totalSteps();
+    };
+
+    const handleNext = () => {
+        const newActiveStep =
+            isLastStep() && !allStepsCompleted()
+                ? // It's the last step, but not all steps have been completed,
+                // find the first step that has been completed
+                steps.findIndex((step, i) => !(i in completed))
+                : activeStep + 1;
+        setActiveStep(newActiveStep);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleStep = (step: number) => () => {
+        setActiveStep(step);
+    };
+
+    const handleComplete = () => {
+        const newCompleted = completed;
+        newCompleted[activeStep] = true;
+        setCompleted(newCompleted);
+        handleNext();
+    };
+
+    const handleReset = () => {
+        setActiveStep(0);
+        setCompleted({});
+    };
+
+    const TextEditorStepper = () => {
+        return (
+            <Box sx={{ width: '100%' }}>
+                <Stepper nonLinear activeStep={activeStep}>
+                    {steps.map((label, index) => (
+                        <Step key={label} completed={completed[index]}>
+                            <StepButton color="inherit" onClick={handleStep(index)}>
+                                {label}
+                            </StepButton>
+                        </Step>
+                    ))}
+                </Stepper>
+            </Box>
+        );
+    }
+
+    const TextEditorStepperActions = () => {
+        return (
+            <div>
+                {allStepsCompleted() ? (
+                    <React.Fragment>
+                        <Typography sx={{ mt: 2, mb: 1 }}>
+                            All steps completed - you&apos;re finished
+                        </Typography>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                            <Box sx={{ flex: '1 1 auto' }} />
+                            <Button onClick={handleReset}>Reset</Button>
+                        </Box>
+                    </React.Fragment>
+                ) : (
+                    <React.Fragment>
+                        <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
+                            Step {activeStep + 1}
+                        </Typography>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                            <Button
+                                color="inherit"
+                                disabled={activeStep === 0}
+                                onClick={handleBack}
+                                sx={{ mr: 1 }}
+                            >
+                                Back
+                            </Button>
+                            <Box sx={{ flex: '1 1 auto' }} />
+                            <Button onClick={handleNext} sx={{ mr: 1 }}>
+                                Next
+                            </Button>
+                            {activeStep !== steps.length &&
+                                (completed[activeStep] ? (
+                                    <Typography variant="caption" sx={{ display: 'inline-block' }}>
+                                        Step {activeStep + 1} already completed
+                                    </Typography>
+                                ) : (
+                                    <Button onClick={handleComplete}>
+                                        {completedSteps() === totalSteps() - 1
+                                            ? 'Finish'
+                                            : 'Complete Step'}
+                                    </Button>
+                                ))}
+                        </Box>
+                    </React.Fragment>
+                )}
+            </div>
+        );
+    }
+
     return (
-        <></>
+        <Card id="duegev-text-editor">
+            <CardContent>
+                {TextEditorStepper()}
+            </CardContent>
+            <CardActions>
+                {TextEditorStepperActions()}
+            </CardActions>
+        </Card>
     );
 }
 
