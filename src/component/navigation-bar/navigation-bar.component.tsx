96e7d2bfc3cd5a0f053b680d8b4e185e82fd7c9e
unknown <vlfmap@gmail.com>
2024-06-17 12:04:11 +0200

    more cleanup+

--- src/component/navigation-bar/navigation-bar.component.tsx
+++ src/component/navigation-bar/navigation-bar.component.tsx
@@ -58,7 +58,7 @@ function NavigationBar() {
                 <p id="credits">
                     &copy; FrancisStudios - All intellectual and distribution rights reserved by author
                     <a href='https://github.com/francisstudios'> github.com/francisstudios</a> | Intellectual
-                    rights reserved for <b>Levente Vendéghegyi</b> under CC 4.0 License
+                    rights reserved for <b>Dynar Software by FSG</b> under CC 4.0 License
                 </p>
             </Box>
         )
