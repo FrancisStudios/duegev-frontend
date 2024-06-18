de2b8a6a9ff01916f3719abbe2ec415e432e71d8
unknown <vlfmap@gmail.com>
2024-06-18 14:21:53 +0200

    dgv

--- src/page/map.page.tsx
+++ src/page/map.page.tsx
@@ -1,25 +1,62 @@
 import '../style/map.page.css';
-import { Card, CardContent } from '@mui/material';
+import { Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from '@mui/material';
 import Timeline from '@mui/lab/Timeline';
-import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineItem, { timelineItemClasses } from '@mui/lab/TimelineItem';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 
 const MapPage = () => {
+
+
+    const TimelineCard = () => {
+        return (
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    sx={{ height: 140 }}
+                    image="duegev-test-map.jpg"
+                    title="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        2431
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Legutóbbi változtatások, amik történtek a térképen:
+                        bla bla bla bla
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">MEGTEKINTÉS</Button>
+                </CardActions>
+            </Card>
+        );
+    }
+
     return (
         <div id="map-page-card-wrapper">
             <Card variant='outlined' id='map-card'>
                 <CardContent>
-                    <Timeline position="right">
+                    <Timeline
+                        sx={{
+                            [`& .${timelineItemClasses.root}:before`]: {
+                                flex: 0,
+                                padding: 0,
+                            },
+                        }}
+                    >
                         <TimelineItem>
                             <TimelineSeparator>
                                 <TimelineDot />
                                 <TimelineConnector />
                             </TimelineSeparator>
-                            <TimelineContent>Eat</TimelineContent>
+                            <TimelineContent>
+                                {TimelineCard()}
+                            </TimelineContent>
                         </TimelineItem>
+
+
                         <TimelineItem>
                             <TimelineSeparator>
                                 <TimelineDot />
